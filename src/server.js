06ea3b80f3d8f9a12efd6bd234a0192ed467a236
juliana <julianacristina780@gmail.com>
2020-06-09 11:36:23 -0300

    Aula 4 - Refatorando as pastas, adicionando servidor, adicionando nunjucks e refatorando o código com a template engine

---
+++ src/server.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//Utilizando Template Engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos da aplicação
+//pagina inicial
+server.get("/", (req, res) => {
+    return res.render("index.html", {title: "Seu marketplace de coleta de resíduos"})
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search-result", (req, res) => {
+    return res.render("search-result.html")
+})
+
+//ligar o servidor
+server.listen(3000)
